1fe118c1ec324845c306cc9cabc0624ca69afabd
Hi Tran <thuchitran@gmail.com>
2024-06-30 22:36:40 +1000

    project set up

---
+++ backend/database/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  HOST: "techsight.c30qmwaau3pm.ap-southeast-2.rds.amazonaws.com",
+  USER: "thuchitran@gmail.com",
+  PASSWORD: "techSight!%",
+  DB: "techsight",
+  DIALECT: "mysql",
+};
