20caab6835148f4a0da87c308fea00b1eb869116
Hi Tran <thuchitran@gmail.com>
2024-07-12 09:16:08 +1000

    create basic UI component

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yellow: "#FFB703",
+        blue: "#023047",
+        grey: "#EEECE7",
+        red: "#BD2E2E",
+        green: "#1B5816",
+        white: "#fff",
+      },
+      backgroundImage: {
+        background: "url('/assets/background_img.jpg')",
+      },
+    },
   },
   plugins: [],
 };
